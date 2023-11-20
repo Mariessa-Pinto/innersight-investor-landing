f154309134d1e41ff433160f0343dd29fa34ab8c
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-20 15:49:05 -0800

    sign up section

--- components/SignUp/index.js
+++ components/SignUp/index.js
@@ -1,6 +1,18 @@
+import styles from './SignUp.module.css'
+
 export default function SignUp() {
-    return(
-        <div>
+    return (
+        <div className={styles.signUp}>
+            <h1 className={styles.header}>Sign up and stay up to date with our newest releases and when our updates launch</h1>
+            <form className={styles.form}>
+                <label for="name" className={styles.label}>First name<p className={styles.red}>*</p></label>
+                <input type='text' id="name" name="name" className={styles.input} />
+                <label for="lastName" className={styles.label}>Last name<p className={styles.red}>*</p></label>
+                <input type='text' id="lastName" name="lastName" className={styles.input} />
+                <label for="email" className={styles.label}>Email<p className={styles.red}>*</p></label>
+                <input type="email" id="email" name="email" className={styles.input} />
+                <input type="submit" value="Sign Up" className={styles.button} />
+            </form>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 import Footer from '@/components/Footer'
+import SignUp from '@/components/SignUp'
 
 
 export default function Home() {
@@ -15,9 +16,25 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.signUp}>
+        <div className={styles.topSection}>
+          <div className={styles.image}>
+            <Image
+              src='/logo/logo.png'
+              alt='innersight logo'
+              width={391}
+              height={90}
+            />
+            <Image
+              src='/images/mascot-graphic.svg'
+              alt='mascots with phone screen example'
+              width={677}
+              height={681}
+            />
+          </div>
+          <div>
+            <SignUp />
+          </div>
           <div className={styles.box}></div>
-
         </div>
         <div className={styles.keyFeatures}>
 
