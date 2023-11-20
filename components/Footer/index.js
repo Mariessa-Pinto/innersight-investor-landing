0bd1690d995753d4e4731d60cc799a43d2d1c678
Mariessa-Pinto <mpinto13@my.bcit.ca>
2023-11-20 14:20:39 -0800

    footer complete and font imported

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,52 +7,52 @@ export default function Footer() {
         <div className={styles.footerContainer}>
             <div className={styles.footer}>
                 <div className={styles.colOne}>
-                    <ul>
-                        <Link href=''><li>About</li></Link>
-                        <Link href=''><li>Our Story</li></Link>
-                        <Link href=''><li>Meet the Team</li></Link>
-                        <Link href=''><li>Blog</li></Link>
+                    <ul className={styles.list}>
+                        <li className={styles.listTitle}>About</li>
+                        <Link href='https://innersight-blog.vercel.app/about' className={styles.link}><li className={styles.listItem}>Our Story</li></Link>
+                        <Link href='https://innersight-blog.vercel.app/team' className={styles.link}><li className={styles.listItem}>Meet the Team</li></Link>
+                        <Link href='https://innersight-blog.vercel.app/' className={styles.link}><li className={styles.listItem}>Blog</li></Link>
                     </ul>
                 </div>
                 <div className={styles.colTwo}>
-                    <ul>
-                        <Link href=''><li>Support</li></Link>
-                        <Link href=''><li>Crisis Spport Resources</li></Link>
-                        <Link href=''><li>Terms of Service</li></Link>
-                        <Link href=''><li>Privacy Policy</li></Link>
+                    <ul className={styles.list}>
+                        <li className={styles.listTitle}>Support</li>
+                        <li className={styles.listItem}>Crisis Spport Resources</li>
+                        <li className={styles.listItem}>Terms of Service</li>
+                        <li className={styles.listItem}>Privacy Policy</li>
                     </ul>
                 </div>
                 <div className={styles.colThree}>
-                    <ul>
-                        <Link href=''><li>Contact Us</li></Link>
-                        <Link href=''><li>contact@innersight.com</li></Link>
-                        <Link href=''><li>3700 Willington Ave,<br />Burnaby BC<br />V5G 3H2</li></Link>
+                    <ul className={styles.list}>
+                        <li className={styles.listTitle}>Contact Us</li>
+                        <li className={styles.listItem}>contact@innersight.com</li>
+                        <li className={styles.listItem}>3700 Willington Ave,<br />Burnaby BC<br />V5G 3H2</li>
                     </ul>
                 </div>
                 <div className={styles.social}>
                     <Image
-                        src={''}
+                        src={'../../assets/icons/instagram.svg'}
                         alt='instagram icon'
                         width={20}
                         height={20}
                     />
                     <Image
-                        src={''}
-                        alt='instagram icon'
-                        width={20}
+                        src={'../../assets/icons/linkedin.svg'}
+                        alt='linkedin icon'
+                        width={18}
+                        height={19}
+                    />
+                    <Image
+                        src={'../../assets/icons/facebook.svg'}
+                        alt='facebook icon'
+                        width={11.3}
                         height={20}
                     />
                     <Image
-                        src={''}
-                        alt='instagram icon'
-                        width={20}
-                        height={20}
-                    />
-                    <Image
-                        src={''}
-                        alt='instagram icon'
-                        width={20}
-                        height={20}
+                        src={'../../assets/icons/twitter.svg'}
+                        alt='twitter icon'
+                        width={21}
+                        height={16}
                     />
                 </div>
             </div>
